873e0d98c14385a54d62354dc0224bb297e018ee
NicolasEsponda <nico_958@hotmail.com>
2024-07-20 14:17:12 -0300

    Add files via upload

---
+++ ejercicios js/index.js
@@ -0,0 +1,146 @@
+// // #### 1. Consola (Console.log, console.error, etc):
+
+// console.log ("Hola mundo");
+
+// console.log ("Nicolas Esponda");
+
+// // console.error(error);
+
+// let resultado = 5 + 3;
+// console.log (resultado);
+
+// resultado = 10 - 7;
+// console.log (resultado);
+
+// var nombre = "Nicolas";
+// console.log (nombre);
+
+// var edad = "28";
+// console.log (edad);
+
+// resultado = 8 * 5;
+// console.log (resultado);
+
+// resultado = 20 / 4;
+// console.log (resultado);
+
+// var a = "10";
+// console.log (a);
+
+// var b = "10";
+// console.log (b);
+
+// resultado = a + b;
+// console.log (resultado);
+
+// let miPrimerTexto = "primer cadena de texto";
+// console.log (miPrimerTexto);
+
+// let miSegundoTexto = "segunda cadena de texto";
+// console.log (miSegundoTexto)
+
+// let variablesConcatenadas = miPrimerTexto + " " + miSegundoTexto;
+// console.log (variablesConcatenadas);
+
+// // #### 2. Prompt:
+
+// let nombreCompleto = prompt("ingrese su nombre")
+// console.log(nombreCompleto)
+
+// let ingreseEdad = prompt("ingrese su edad")
+// console.log(ingreseEdad)
+
+// let primerNumero = prompt("ingrese un numero")
+// let segundoNumero = prompt("ingrese un numero")
+// let suma = primerNumero + segundoNumero;
+// console.log(suma)
+
+// let colorFavorito = prompt("Ingresa tu color favorito:");
+// console.log(colorFavorito)
+
+// let frase = prompt("ingrese una frase")
+// console.log(frase)
+
+// let ciudadDeNacimiento = prompt("ingrese su ciudad de nacimiento")
+// console.log(ciudadDeNacimiento)
+
+// let peliculaFavorita = prompt("ingrese su pelicula favorita")
+// console.log(peliculaFavorita)
+
+// let mascota = prompt("ingrese el nombre de su mascota")
+// console.log(mascota)
+
+// let deporteFavorito = prompt("ingrese su deporte favorito")
+// console.log(deporteFavorito)
+
+// let cancionFavorita =prompt("ingrese su cancion favorita")
+// console.log(cancionFavorita)
+
+// // #### 3. Alert:
+
+// const textoBienvenida = "bienvenidos"
+// alert(textoBienvenida)
+
+// let comidaFavorita =prompt("ingrese su comida favorita")
+// console.log(comidaFavorita)
+// alert(comidaFavorita)
+
+// const buenosDias = "¡buenos dias!"
+// alert(buenosDias)
+
+// const buenasTardes = "¡buenas tardes!"
+// alert(buenasTardes)
+
+// const buenasNoches = "¡buenas noches!"
+// alert(buenasNoches)
+
+// let animalFavorito =prompt("ingrese su animal favorito")
+// console.log(animalFavorito)
+// alert(animalFavorito)
+
+// const felizCumpleanios = "¡Feliz cumpleaños!"
+// alert(felizCumpleanios)
+
+// let bebidaFavorita =prompt("ingrese su bebida favorita")
+// console.log(bebidaFavorita)
+// alert(bebidaFavorita)
+
+// const felizAnioNuevo = "¡Feliz año nuevo!"
+// alert(felizAnioNuevo)
+
+// let estacionFavorita =prompt("ingrese su estacion favorita")
+// console.log(estacionFavorita)
+// alert(estacionFavorita)
+
+// #### 4. Combinación de Consola, Prompt y Alert
+
+// let nombre = prompt("ingrese su nombre")
+// console.log(nombre)
+// let edad =prompt("ingrese su edad")
+// console.log(edad)
+
+// alert(nombre + " " + edad)
+
+// const anioActual = "2024"
+// let anioDeNacimiento = anioActual - edad;
+// console.log(anioDeNacimiento)
+
+// if (edad >= 18) {
+//     alert("Eres mayor de edad.");
+// } else {
+//     alert("Eres menor de edad.");
+// }
+
+// let numero1 = prompt("ingrese el primer numero")
+// let numero2 =prompt("ingrese el segundo numero")
+// let suma = numero1 + numero2;
+// alert(suma);
+
+// const hola = "Hola,"
+// let nombre = prompt("ingrese su nombre")
+// alert(hola + nombre)
+
+
+// let anioDeNacimiento =prompt("ingrese su año de nacimiento")
+// console.log(anioDeNacimiento)
+// alert(anioDeNacimiento)
